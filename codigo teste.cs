25710368263340c26b44f933bc4b2b722ad687a0
LucasDaniel19 <lucasrodriguesdaniel13k@gmail.com>
2024-03-01 16:19:02 -0300

    criação do arquivo codigo teste
    durante eu creiei o primeiro codigo

---
+++ codigo teste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Repositorio_Nome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Lucas";
+            Console.WriteLine (nome);
+        }
+    }
+}
